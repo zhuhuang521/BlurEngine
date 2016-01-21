b067a575bf1a0dce94eacd875cc90b0b6188e60a
xuesong.zhu <xuesong.zhu@qunar.com>
2016-01-21 14:01:24 +0800

    change jni to so

--- app/src/main/jni/Application.mk
+++
@@ -1,5 +0,0 @@
-# The ARMv7 is significanly faster due to the use of the hardware FPU
-#APP_ABI := armeabi armeabi-v7a arm64-v8a
-#TARGET_ARCH_ABI := arm64-v8a
-APP_ABI := armeabi
-APP_PLATFORM := android-19
